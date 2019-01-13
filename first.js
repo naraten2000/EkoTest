d4000a096654e7344594c170eac40176d71697b6
Narate Nantaburom <naraten2000@yahoo.com>
2019-01-13 19:17:07 +0700

    Added couple test files.

---
+++ first.js
@@ -0,0 +1,42 @@
+﻿'use strict';
+
+var array = ['AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 'MORA', 'ESPONJA', 'RAMO', 'JAPONES', 'ARMO', 'MOJAN', 'MARO', 'ORAM', 'MONJA', 'ALEXIS'];
+array.sort();
+
+var resultArray = [''];
+
+array.forEach(element => {
+    if (resultArray.indexOf(element) === -1) {
+
+        var length = element.length;
+        var resultArrayInternal = [];
+
+        array.forEach(elementInternal => {
+            if (elementInternal.length === length) {
+
+                resultArray.push(elementInternal);
+
+                var identicalFlag = true;
+
+                for (var i = 0; i < element.length; i++) {
+                    if (elementInternal.indexOf(element.charAt(i)) === -1) {
+                        identicalFlag = false;
+                        break;
+                    }
+                };
+
+                if (identicalFlag)
+                    resultArrayInternal.push(elementInternal);
+            }
+        });
+
+        console.log(resultArrayInternal.join('-'))
+    }
+});
+
+
+function function2() {
+    // all the stuff you want to happen after that pause
+    console.log('Blah blah blah blah extra-blah');
+}
+setTimeout(function2, 11000);
---
+++ second.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+var testString = 'foo(bar)blim';
+
+var leftPosition = testString.indexOf('(');
+var rightPosition = testString.indexOf(')');
+var textInParentheses = testString.substr(leftPosition + 1, rightPosition - leftPosition - 1);
+
+var newTextInParentheses = '';
+for (var i = 0; i < textInParentheses.length; i++) {
+    newTextInParentheses += textInParentheses[textInParentheses.length - i - 1];
+}
+
+var finalText = testString.substr(0, leftPosition - 1) + newTextInParentheses + testString.substr(rightPosition + 1);
+
+console.log(finalText);
+
